b7f3409435e84d33940fb42f88b2a0b87d8821b4
Néstor De La Cruz <100051769+nestordelacruz@users.noreply.github.com>
2023-03-08 20:19:30 -0800

    Finished login form interface

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,26 +1,42 @@
 
-<div class="card flex justify-center items-center">
-    <div class="p-4">
+<div class="flex justify-center"> 
+<div class="card m-10 flex items-center  max-w-[50%] justify-center ">
+    <div class="p-4 ">
+        <form>
         <div class="space-y-4">
             <label class="label">
                 <span>Nombre de usuario</span>
-                <input class="input" type="text" placeholder="Ingrese nombre de usuario" />
+                <input class="input" type="text" placeholder="Ingrese nombre de usuario" required/>
             </label>
             <label class="label">
                 <span>Nombre </span>
-                <input class="input" type="text" placeholder="Ingrese nombre" />
+                <input class="input" type="text" placeholder="Ingrese nombre" required />
             </label>
             <label class="label">
                 <span>Apellido </span>
-                <input class="input" type="text" placeholder="Ingrese apellido" />
+                <input class="input" type="text" placeholder="Ingrese apellido" required />
+            </label>
+            <label class="label">
+                <span>Teléfono</span>
+                <input class="input" type="tel" placeholder="Ingrese teléfono" required />
             </label>
             <label class="label">
                 <span>Correo electrónico</span>
-                <input class="input" type="email" placeholder="john@ejemplo.com" autocomplete="email" />
+                <input class="input" type="email" placeholder="john@ejemplo.com" autocomplete="email" required/>
+            </label>
+            <label class="label">
+                <span>Contraseña</span>
+                <input class="input" type="password" placeholder="Ingrese contraseña" required/>
+            </label>
+            <label class="label">
+                <span>Confirme contraseña</span>
+                <input class="input" type="password" placeholder="Reingrese contraseña" required/>
             </label>
         </div>
         <div class="flex py-8 space-y-4 justify-center items-center">
             <button class="btn variant-filled-primary">Submit</button>
         </div>
+        </form>
     </div>
+</div>
 </div>
